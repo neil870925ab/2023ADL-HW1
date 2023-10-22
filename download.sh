2088a7a7bc49b4325bf781bf0160000b0f4a4e28
neil870925ab <52836478+neil870925ab@users.noreply.github.com>
2023-10-22 21:48:45 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget "https://www.dropbox.com/scl/fo/dm6dbgtzizen5voist0z3/h?rlkey=wuus8uhhqfzbjlpds7snwjqpj&dl=1" -O Model.zip
-unzip Model.zip
+wget "https://www.dropbox.com/scl/fo/dm6dbgtzizen5voist0z3/h?rlkey=86enqgbxpz526ms7j89k6u14o&dl=1" -O download.zip
+unzip download.zip
