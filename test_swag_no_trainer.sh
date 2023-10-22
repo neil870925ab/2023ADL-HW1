cc961c2495d26d9026a97954e191ed837d85e500
neil870925ab <52836478+neil870925ab@users.noreply.github.com>
2023-10-22 16:30:59 +0800

    Update test_swag_no_trainer.sh

--- test_swag_no_trainer.sh
+++ test_swag_no_trainer.sh
@@ -1,5 +1,5 @@
 python ./test_swag_no_trainer.py \
-  --model_name_or_path ./MC_model \
+  --model_name_or_path ./Model/Multiple_Choice \
   --pad_to_max_length \
   --context_file "${1}" \
   --test_file "${2}" \
