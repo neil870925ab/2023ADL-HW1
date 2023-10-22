1ea162fd86cf3e81fabed76cf272acd0d9871888
neil870925ab <52836478+neil870925ab@users.noreply.github.com>
2023-10-22 16:03:16 +0800

    Create run_swag_no_trainer.sh

---
+++ run_swag_no_trainer.sh
@@ -0,0 +1,7 @@
+python ./run_swag_no_trainer.py \
+  --model_name_or_path hfl/chinese-lert-base \
+  --pad_to_max_length \
+  --context_file "${1}" \
+  --train_file "${2}" \
+  --validation_file "${3}" \
+  --output_dir ./MC_model 
